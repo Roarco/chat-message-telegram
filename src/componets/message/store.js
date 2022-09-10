e75ad0a54fa947ebd3c4c7820d4d0c609887af27
Roarco <sebuscaroberth@gmail.com>
2022-09-10 16:44:39 -0500

    MongoDB: Almacenar y leer datos

--- src/componets/message/store.js
+++ src/componets/message/store.js
@@ -1,12 +1,29 @@
 
-const list = [];
+const mongoose = require("mongoose");
+const Model = require("./model");
+
+const username = 'roarco';
+const password = '1104017400';
+const dbName = 'MessageDB';
+const uri = `mongodb+srv://${username}:${password}@cluster0.r5gkvxf.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Conectado a la base de datos');
+    })
+    .catch((err) => {
+        console.log('Error al conectar a la base de datos', err);
+    });
+
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessage() {
-    return list;
+async function getMessage() {
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
