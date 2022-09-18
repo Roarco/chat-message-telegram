9c9a5f8229f59c71a0b9d6bb69fa112f1424f48a
Roarco <sebuscaroberth@gmail.com>
2022-09-17 19:08:54 -0500

    recreando el gitignore

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,14 @@ require('dotenv').config({
 });
 
 const config = {
+	env: process.env.NODE_ENV || 'development',
+	isProd: process.env.NODE_ENV === 'production',
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	host: process.env.HOST,
 	dbHost: process.env.DB_HOST,
 	dbName: process.env.DB_NAME,
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 3001,
 	uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 };
 
