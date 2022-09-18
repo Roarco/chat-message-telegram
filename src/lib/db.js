48389ee6fd68ac202c51e7a462051dd0bdc073b7
Roarco <sebuscaroberth@gmail.com>
2022-09-17 22:22:05 -0500

    prueba fix bug error en deploy prod

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,10 +4,12 @@ const chalk = require('chalk');
 
 const connect = async () => {
     try{
+        console.log(config.uri);
         await mongoose.connect(config.uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log(chalk.green('Database connected'));
     }catch(err){
         console.log(chalk.red('Database connection failed'));
+        console.log(err);
     }
 }
 
