a2ed281f420021c7db96e9d0339cbd6c5dbcb5ff
Roarco <sebuscaroberth@gmail.com>
2022-09-17 19:38:25 -0500

    Prueba para produccion con la uri

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const config = require('../config');
 const chalk = require('chalk');
 
+const uri = `mongodb+srv://roarco:1104017400@cluster0.r5gkvxf.mongodb.net/MessageDB?retryWrites=true&w=majority`;
 
 const connect = async () => {
     try{
-        await mongoose.connect(config.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log(chalk.green('Database connected'));
     }catch(err){
         console.log(chalk.red('Database connection failed'));
